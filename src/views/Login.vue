e8b8b61668c024b90678b61c29bea54811306d37
lz7git <happyli742@163.com>
2019-12-08 22:58:50 +0800

    zxcv

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="login">
+    <div class="box"></div>
   </div>
 </template>
 
 <style scoped>
 .login{
-  height: 100vw;
+  height: 100vh;
   width: 100vw;
   background-image: url("../assets/images/login.jpg");
-  background-size:100% 100%;
+  background-size: cover;
   background-repeat:no-repeat;
+  background-position: center;
+  overflow: hidden;
+}
+.login .box {
+  width: 300px;
+  height: 400px;
+  background-color: white;
 }
 </style>
