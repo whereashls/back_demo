e88fb41cf281a76c5ef01a6ba41a2454b01b0be6
whereashls <1183676168@qq.com>
2019-12-08 22:30:35 +0800

    seven

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="login">
+    <div class="box"></div>
   </div>
 </template>
 
 <style scoped>
 .login{
-  height: 100vw;
+  height: 100vh;
   width: 100vw;
   background-image: url("../assets/images/login.jpg");
-  background-size:100% 100%;
+  background-size: cover;
   background-repeat:no-repeat;
+  background-position:center;
+  overflow: hidden;
+  display:flex;
+  justify-content: center;
+  align-items: center
+}
+.login .box{
+  width: 200px;
+  height: 200px;
+  background: white;
 }
 </style>
