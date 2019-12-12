61db22b51186365a419fbb19ac8765ad8386d2b4
whereashls <1183676168@qq.com>
2019-12-12 22:49:37 +0800

    2

--- src/views/Home/TeachersManagement.vue
+++ src/views/Home/TeachersManagement.vue
@@ -3,17 +3,35 @@
     <div class="content">
       <div class="content-header">
         <span>共{{list.total}}条结果</span>
-        <el-button @button="add" size="medium" type="primary">新增</el-button>
+        <el-button  size="medium" type="primary">新增</el-button>
       </div>
 
-      <el-table :data="list.data" border v-loading="this.loading">
+      <el-table :data="list.data" border>
         <el-table-column prop="d_name" label="系别名称"></el-table-column>
         <el-table-column label="操作" width="150">
           <template slot-scope="scope">
             <el-button size="mini" @click="update(scope.row.id)">编辑</el-button>
+            <el-button size="mini" type="danger" @click="remove(scope.row.id)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      list: {
+        data: [],
+        tatol: 0,
+        loading: false
+      }
+    }
+  }
+  // created () {
+  //   this.getList()
+  // }
+}
+</script>
