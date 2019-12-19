25138b9bc27731ce9fe68eaff05a58e9105907c7
whereashls <1183676168@qq.com>
2019-12-19 21:31:03 +0800

    -

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,5 +1,7 @@
 import Axios from 'axios'
 
+import store from '@/store'
+
 const axios = Axios.create({
   baseURL: 'http://122.51.6.249:3001/'
 })
@@ -9,7 +11,11 @@ axios.interceptors.request.use(config => {
   let url = config.url.split('/')
   if (url[url.length] !== 'login') {
     // config.headers.common['Authorization'] = localStorage.getItem('token')
-    config.headers.common['Authorization'] = this.store.$state.getItem('token')
+    // config.headers.common['Authorization'] = this.store.$state.getItem('token')
+    config.headers.common['Authorization'] = store.state.user.token
+    // config.headers.common['helloworld'] = 'hello'
+    // console.log('store:')
+    // console.log(store)
   }
   return config
 }, error => {
